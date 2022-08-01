404da1b6d4543b49ae1353b821f200a524cf1d1a
KeisukeTaniguchi <ta22guchi@gmail.com>
2022-08-01 13:45:24 +0900

    AWSAmplifyで認証機能を試す

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
+import { AmplifySignOut, AmplifyAuthenticator } from '@aws-amplify/ui-react'
+import Amplify, { I18n } from 'aws-amplify'
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
+import awsconfig from './aws-exports'
+import { vocabularies } from './assets/amplify/vocabularies'
+
+I18n.putVocabularies(vocabularies)
+I18n.setLanguage('ja')
+Amplify.configure(awsconfig) // eslint-disable-line
 
 function App() {
-  return (
+  const [authState, setAuthState] = useState()
+  const [user, setUser] = useState()
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState)
+      setUser(authData)
+    })
+  }, [])
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AmplifySignOut />
+      <h2>コンポーネント</h2>
+      <p>{user.attributes.email}</p>
     </div>
-  );
+  ) : (
+    <AmplifyAuthenticator />
+  )
 }
 
 export default App;
